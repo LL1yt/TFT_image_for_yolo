68b59876c7262aa0b4f244ca84127e94ea682b01
n0name <pyst0e@me.com>
2024-03-09 00:16:26 +0300

    side menu By CodingNepal

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,10 +1,22 @@
-$(document).ready(() => {
-    const sidebarToggle = $('#sidebar-toggle');
-    const sidebar = $('#sidebar');
-    const content = $('#content');
+const body = document.querySelector('body'),
+    sidebar = body.querySelector('nav'),
+    toggle = body.querySelector(".toggle"),
+    searchBtn = body.querySelector(".search-box"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text");
+toggle.addEventListener("click", () => {
+    sidebar.classList.toggle("close");
+})
+searchBtn.addEventListener("click", () => {
+    sidebar.classList.remove("close");
+})
+modeSwitch.addEventListener("click", () => {
+    body.classList.toggle("dark");
 
-    sidebarToggle.click(() => {
-        sidebar.toggleClass('active');
-        content.toggleClass('active');
-    });
+    if (body.classList.contains("dark")) {
+        modeText.innerText = "Light mode";
+    } else {
+        modeText.innerText = "Dark mode";
+
+    }
 });
