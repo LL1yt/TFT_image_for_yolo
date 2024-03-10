64b4529ce0d5822270293c9fbc44d70cfbce80bd
n0name <pyst0e@me.com>
2024-03-10 19:29:47 +0300

    доработка бокового меню

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -15,3 +15,34 @@ modeSwitch.addEventListener("click", () => {
         modeText.innerText = "Dark mode";
     }
 });
+document.addEventListener('DOMContentLoaded', function () {
+    // Получаем все элементы меню, которые имеют подменю
+    const hasSubmenu = document.querySelectorAll('.nav-link.has-submenu');
+
+    // Добавляем обработчик событий для каждого элемента меню с подменю
+    hasSubmenu.forEach(link => {
+        link.addEventListener('click', function (event) {
+            // Предотвращаем стандартное поведение ссылки
+            event.preventDefault();
+
+            // Закрываем все открытые подменю
+            hasSubmenu.forEach(item => {
+                if (item !== link) {
+                    item.classList.remove('submenu-active');
+                }
+            });
+
+            // Переключаем видимость подменю для текущего элемента меню
+            link.classList.toggle('submenu-active');
+        });
+    });
+});
+
+// Закрытие подменю при клике вне меню
+document.addEventListener('click', function (event) {
+    if (!event.target.closest('.sidebar')) {
+        hasSubmenu.forEach(link => {
+            link.classList.remove('submenu-active');
+        });
+    }
+});
