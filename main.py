31741702e070f64cc15484574a41be115065159f
n0name <pyst0e@me.com>
2024-03-11 00:11:20 +0300

    база для ajax and jinja

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    title = "Modern Flask Site"
+    return render_template("index.html", title=title)
+
+
+@app.route("/_get_data", methods=["POST"])
+def get_data():
+    myList = [
+        "element1",
+        "element2",
+        "element3",
+    ]
+    return jsonify({"data": render_template("about.html", myList=myList)})
 
 
 if __name__ == "__main__":
--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,48 +1,58 @@
-const body = document.querySelector('body'),
-    sidebar = body.querySelector('nav'),
-    toggle = body.querySelector(".toggle"),
-    modeSwitch = body.querySelector(".toggle-switch"),
-    modeText = body.querySelector(".mode-text");
+const body = document.querySelector("body"),
+  sidebar = body.querySelector("nav"),
+  toggle = body.querySelector(".toggle"),
+  modeSwitch = body.querySelector(".toggle-switch"),
+  modeText = body.querySelector(".mode-text");
 toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-})
-modeSwitch.addEventListener("click", () => {
-    body.classList.toggle("dark");
-
-    if (body.classList.contains("dark")) {
-        modeText.innerText = "Light mode";
-    } else {
-        modeText.innerText = "Dark mode";
-    }
+  sidebar.classList.toggle("close");
 });
-document.addEventListener('DOMContentLoaded', function () {
-    // Получаем все элементы меню, которые имеют подменю
-    const hasSubmenu = document.querySelectorAll('.nav-link.has-submenu');
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
 
-    // Добавляем обработчик событий для каждого элемента меню с подменю
-    hasSubmenu.forEach(link => {
-        link.addEventListener('click', function (event) {
-            // Предотвращаем стандартное поведение ссылки
-            event.preventDefault();
+  if (body.classList.contains("dark")) {
+    modeText.innerText = "Light mode";
+  } else {
+    modeText.innerText = "Dark mode";
+  }
+});
+document.addEventListener("DOMContentLoaded", function () {
+  // Получаем все элементы меню, которые имеют подменю
+  const hasSubmenu = document.querySelectorAll(".nav-link.has-submenu");
 
-            // Закрываем все открытые подменю
-            hasSubmenu.forEach(item => {
-                if (item !== link) {
-                    item.classList.remove('submenu-active');
-                }
-            });
+  // Добавляем обработчик событий для каждого элемента меню с подменю
+  hasSubmenu.forEach((link) => {
+    link.addEventListener("click", function (event) {
+      // Предотвращаем стандартное поведение ссылки
+      event.preventDefault();
 
-            // Переключаем видимость подменю для текущего элемента меню
-            link.classList.toggle('submenu-active');
-        });
+      // Закрываем все открытые подменю
+      hasSubmenu.forEach((item) => {
+        if (item !== link) {
+          item.classList.remove("submenu-active");
+        }
+      });
+
+      // Переключаем видимость подменю для текущего элемента меню
+      link.classList.toggle("submenu-active");
     });
+  });
 });
 
 // Закрытие подменю при клике вне меню
-document.addEventListener('click', function (event) {
-    if (!event.target.closest('.sidebar')) {
-        hasSubmenu.forEach(link => {
-            link.classList.remove('submenu-active');
-        });
-    }
+document.addEventListener("click", function (event) {
+  if (!event.target.closest(".sidebar")) {
+    hasSubmenu.forEach((link) => {
+      link.classList.remove("submenu-active");
+    });
+  }
+});
+
+$("#likes").click(() => {
+  $.ajax({
+    url: "/_get_data",
+    type: "POST",
+    success: (response) => {
+      $("div.content").append(response.data);
+    },
+  });
 });
